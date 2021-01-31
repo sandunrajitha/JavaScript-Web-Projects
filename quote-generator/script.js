98ce6bca8cb21e6fa9645b7e962303b37bf3dd66
Sandun Rajitha <rajitha1911@gmail.com>
2021-01-31 16:16:35 +0530

    add cors-anywhere proxy server to fix the CORS error

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,13 +1,31 @@
+let quotesList = [];
+const proxyURL = 'https://cors-anywhere.herokuapp.com/';
+// const url = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+const url = 'http://type.fit/api/quotes';
+
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorName = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+// New Quote
+
+function newQuote() {
+    let quoteNumber = Math.floor(Math.random() * quotesList.length);
+}
+
 // Get Quote from API
 
-async function getQuote(){
-    const url = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
+async function getQuote() {
+
+    const response = await fetch(proxyURL + url);
+    quotesList = await response.json();
+    
     try {
 
     } catch (error) {
+        getQuote();
         console.log('error getting quote', error);
     }
 }
