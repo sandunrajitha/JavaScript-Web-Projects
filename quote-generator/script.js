273b3f1bfde8969a750b01c5c8f0005d14a6748d
Sandun Rajitha <rajitha1911@gmail.com>
2021-01-31 12:35:27 +0530

    get quotes from the API

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,1 +1,17 @@
-console.log('This will be awesome <3')
+// Get Quote from API
+
+async function getQuote(){
+    const url = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    try {
+
+    } catch (error) {
+        console.log('error getting quote', error);
+    }
+}
+
+// On Load
+
+getQuote();
