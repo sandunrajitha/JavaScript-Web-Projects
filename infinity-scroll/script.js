6485ce9b094d3457c04d8f460fa7575c21dc65c1
Sandun Rajitha <rajitha1911@gmail.com>
2021-02-07 14:50:06 +0530

    make fetch request and populate the page with response images

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -1,1 +1,43 @@
-console.log('this will be awesome <3')
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
+const count = 10;
+const resource = `https://rajitha-api-proxy.herokuapp.com/api/infinite-scroll?count=${count}`
+
+// Create photo elements for each object in the photos array
+
+function displayPhotos() {
+    photosArray.forEach((photo) => {
+        const item = document.createElement('a');
+        item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank');
+
+        const img = document.createElement('img');
+        img.setAttribute('src', photo.urls.regular);
+        img.setAttribute('alt', photo.alt_description);
+        img.setAttribute('title', photo.alt_description);
+
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+
+    });
+}
+
+// Get photos
+
+async function getPhotos() {
+    try {
+        const response = await fetch(resource);
+        photosArray = await response.json();
+        console.log(photosArray);
+        displayPhotos();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// on load
+
+getPhotos();
