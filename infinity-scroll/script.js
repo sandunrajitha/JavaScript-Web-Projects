93185be90a6db1f8ca086658a6a47fc845545ebc
Sandun Rajitha <rajitha1911@gmail.com>
2021-02-07 15:06:44 +0530

    create helper function to set element attributes

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -6,19 +6,29 @@ let photosArray = [];
 const count = 10;
 const resource = `https://rajitha-api-proxy.herokuapp.com/api/infinite-scroll?count=${count}`
 
+// helper function for set attributes
+
+function setAttributes(element, attributes) {
+    for(const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
 // Create photo elements for each object in the photos array
 
 function displayPhotos() {
     photosArray.forEach((photo) => {
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
-
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        });
+        
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
-
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        });
         item.appendChild(img);
         imageContainer.appendChild(item);
 
